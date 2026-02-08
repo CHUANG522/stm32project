e6f3763527ec54d50cdc1b87702565a9b686fad6
CHUANG <3418732870@qq.com>
2026-02-08 22:21:22 +0800

    使用printf进行串口通信

--- test1/MyCode/fun.c
+++ test1/MyCode/fun.c
@@ -25,22 +25,22 @@ void led_show(uint8_t led ,uint8_t mode )
 	
 }
 
-uint16_t B1_state ;//按键1当前状态
-uint16_t B1_last_state =1; //按键1上一次的状态
-uint16_t B2_state ;
-uint16_t B2_last_state =1;
+uint16_t Key_B1_state ;//按键1当前状态
+uint16_t Key_B1_last_state =1; //按键1上一次的状态4
+uint16_t Key_B2_state ;
+uint16_t Key_B2_last_state =1;
 void key_scan(void)
 {
-	B1_state = HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_2) ;
+	Key_B1_state = HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_2) ;
 	
-	if( B1_state == 0 && B1_last_state == 1)  led_show(5,1);
+	if( Key_B1_state == 0 && Key_B1_last_state == 1)  led_show(5,1);
 	//条件，按键按下时当前为0，那么上一次为1
-	B1_last_state = B1_state ; 
+	Key_B1_last_state = Key_B1_state ; 
 	//使用结束将当前状态保留
-	B2_state = HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_4 ) ;
+	Key_B2_state = HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_4 ) ;
 	
-	if( B2_state == 1 && B2_last_state == 0)  led_show(5,0);
+	if( Key_B2_state == 1 && Key_B2_last_state == 0)  led_show(5,0);
 	
-	B2_last_state = B2_state ;
+	Key_B2_last_state = Key_B2_state ;
 }
 
